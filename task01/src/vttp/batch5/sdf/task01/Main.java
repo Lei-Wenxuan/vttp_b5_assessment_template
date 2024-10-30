7ac19ca1b2cd7279e8ee3051d6cd9e6ca9526e30
Lei-Wenxuan <leiwenxuan@gmail.com>
2024-10-30 13:14:57 +0800

    task01 updates

--- task01/src/vttp/batch5/sdf/task01/Main.java
+++ task01/src/vttp/batch5/sdf/task01/Main.java
@@ -19,8 +19,7 @@ import vttp.batch5.sdf.task01.models.BikeEntry;
 
 public class Main {
 
-	public static final String[] POSITION = { "highest", "second highest", "third highest", "fourth highest",
-			"fifth highest" };
+	public static final String[] POSITION = { "highest", "second highest", "third highest", "fourth highest", "fifth highest" };
 	public static final String[] WEATHER = { "Clear, Few clouds, Partly cloudy, Partly cloudy",
 			"Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist",
 			"Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds",
